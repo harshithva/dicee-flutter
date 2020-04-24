3daecf482df930326022b9cd1471a9011a05873f
harshithva <harshith11032001@gmail.com>
2020-04-24 11:41:59 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Color(0xff192a56),
+      appBar: AppBar(
+        title: Text('Dicee'),
+        backgroundColor: Color(0xff273c75),
+      ),
+      body: DicePage(),
+    ));
+  }
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+      ],
+    );
+  }
+}
